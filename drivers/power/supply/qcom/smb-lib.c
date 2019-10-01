aosp-mirror/kernel_msm

smb-lib: Add support for POWER_SUPPLY_TYPEC_PR_SOURCE_1_5
Once the contract is established, the pullup should be either
Rp-1.5 o Rp-3.0. This commit provides support for Rp-1.5
once by writing into TYPE_C_CFG_2_REG:EN_80UA_180UA_CUR_SOURCE_BIT.

Bug: 77704125
Change-Id: I3a6bb8d2f5fed2f2a1afff26e7cd9613e8769d34
Signed-off-by: Badhri Jagan Sridharan <badhri@google.com>

drivers/power/supply/qcom/smb-lib.c
@@ -2447,7 +2447,7 @@ int smblib_get_prop_typec_power_role(struct smb_charger *chg,
 				     union power_supply_propval *val)
 {
 	int rc = 0;
-	u8 ctrl;
+	u8 ctrl, isrc;
 
 	rc = smblib_read(chg, TYPE_C_INTRPT_ENB_SOFTWARE_CTRL_REG, &ctrl);
 	if (rc < 0) {
@@ -2457,6 +2457,12 @@ int smblib_get_prop_typec_power_role(struct smb_charger *chg,
 	}
 	smblib_dbg(chg, PR_REGISTER, "TYPE_C_INTRPT_ENB_SOFTWARE_CTRL = 0x%02x\n",
 		   ctrl);
+
+	rc = smblib_read(chg, TYPE_C_CFG_2_REG, &isrc);
+	if (rc < 0) {
+		smblib_err(chg, "Couldn't read TYPE_C_CFG_2_REG rc=%d\n", rc);
+		return rc;
+	}
 
 	if (ctrl & TYPEC_DISABLE_CMD_BIT) {
 		val->intval = POWER_SUPPLY_TYPEC_PR_NONE;
@@ -2468,7 +2474,9 @@ int smblib_get_prop_typec_power_role(struct smb_charger *chg,
 		val->intval = POWER_SUPPLY_TYPEC_PR_DUAL;
 		break;
 	case DFP_EN_CMD_BIT:
-		val->intval = POWER_SUPPLY_TYPEC_PR_SOURCE;
+		val->intval = isrc & EN_80UA_180UA_CUR_SOURCE_BIT ?
+			POWER_SUPPLY_TYPEC_PR_SOURCE_1_5 :
+			POWER_SUPPLY_TYPEC_PR_SOURCE;
 		break;
 	case UFP_EN_CMD_BIT:
 		val->intval = POWER_SUPPLY_TYPEC_PR_SINK;
@@ -2745,6 +2753,7 @@ static int smblib_set_prop_typec_power_role_locked(
 		reg = UFP_EN_CMD_BIT | EXIT_SNK_BASED_ON_CC_BIT;
 		break;
 	case POWER_SUPPLY_TYPEC_PR_SOURCE:
+	case POWER_SUPPLY_TYPEC_PR_SOURCE_1_5:
 		reg = DFP_EN_CMD_BIT;
 		break;
 	default:
@@ -2767,6 +2776,14 @@ static int smblib_set_prop_typec_power_role_locked(
 				rc);
 		}
 	}
+
+	rc = smblib_masked_write(chg, TYPE_C_CFG_2_REG,
+				 EN_80UA_180UA_CUR_SOURCE_BIT,
+				 pr_role == POWER_SUPPLY_TYPEC_PR_SOURCE_1_5
+				 ? EN_80UA_180UA_CUR_SOURCE_BIT : 0);
+
+	if (rc < 0)
+		smblib_err(chg, "Couldnt update EN_ISRC_180UA_BIT rc=%d\n", rc);
 
 	rc = smblib_masked_write(chg, TYPE_C_INTRPT_ENB_SOFTWARE_CTRL_REG,
 				 TYPEC_POWER_ROLE_CMD_MASK |

ZVNexus/android_kernel_google_bonito

Bug: 80101091
Change-Id: Id603f0ba2b564a4aefc7f6b6556eeb09d2146c68

drivers/power/supply/qcom/smb-lib.c
@@ -1150,6 +1150,7 @@ static int __smblib_set_prop_typec_power_role(struct smb_charger *chg,
 		power_role = UFP_EN_CMD_BIT;
 		break;
 	case POWER_SUPPLY_TYPEC_PR_SOURCE:
+	case POWER_SUPPLY_TYPEC_PR_SOURCE_1_5:
 		power_role = DFP_EN_CMD_BIT;
 		break;
 	default:
@@ -1174,6 +1175,14 @@ static int __smblib_set_prop_typec_power_role(struct smb_charger *chg,
 			}
 		}
 	}
+
+	rc = smblib_masked_write(chg, TYPE_C_CFG_2_REG,
+				 EN_80UA_180UA_CUR_SOURCE_BIT,
+				 power_role == POWER_SUPPLY_TYPEC_PR_SOURCE_1_5
+				 ? EN_80UA_180UA_CUR_SOURCE_BIT : 0);
+
+	if (rc < 0)
+		smblib_err(chg, "Couldnt update EN_ISRC_180UA_BIT rc=%d\n", rc);
 
 	rc = smblib_masked_write(chg, TYPE_C_INTRPT_ENB_SOFTWARE_CTRL_REG,
 				 TYPEC_POWER_ROLE_CMD_MASK, power_role);
@@ -2624,7 +2633,7 @@ int smblib_get_prop_typec_power_role(struct smb_charger *chg,
 				     union power_supply_propval *val)
 {
 	int rc = 0;
-	u8 ctrl;
+	u8 ctrl, isrc;
 
 	rc = smblib_read(chg, TYPE_C_INTRPT_ENB_SOFTWARE_CTRL_REG, &ctrl);
 	if (rc < 0) {
@@ -2634,6 +2643,12 @@ int smblib_get_prop_typec_power_role(struct smb_charger *chg,
 	}
 	smblib_dbg(chg, PR_REGISTER, "TYPE_C_INTRPT_ENB_SOFTWARE_CTRL = 0x%02x\n",
 		   ctrl);
+
+	rc = smblib_read(chg, TYPE_C_CFG_2_REG, &isrc);
+	if (rc < 0) {
+		smblib_err(chg, "Couldn't read TYPE_C_CFG_2_REG rc=%d\n", rc);
+		return rc;
+	}
 
 	if (ctrl & TYPEC_DISABLE_CMD_BIT) {
 		val->intval = POWER_SUPPLY_TYPEC_PR_NONE;
@@ -2645,7 +2660,9 @@ int smblib_get_prop_typec_power_role(struct smb_charger *chg,
 		val->intval = POWER_SUPPLY_TYPEC_PR_DUAL;
 		break;
 	case DFP_EN_CMD_BIT:
-		val->intval = POWER_SUPPLY_TYPEC_PR_SOURCE;
+		val->intval = isrc & EN_80UA_180UA_CUR_SOURCE_BIT ?
+			POWER_SUPPLY_TYPEC_PR_SOURCE_1_5 :
+			POWER_SUPPLY_TYPEC_PR_SOURCE;
 		break;
 	case UFP_EN_CMD_BIT:
 		val->intval = POWER_SUPPLY_TYPEC_PR_SINK;